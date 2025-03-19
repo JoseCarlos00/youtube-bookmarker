730b9b21ea7ac606a82f43a59310ceb344556ce1
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2025-03-19 00:32:59 -0600

    refactor: background se implemento la opcion de reintento automatico para verificar si content.js esta disponible

--- background.js
+++ background.js
@@ -1,11 +1,24 @@
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
-	if (tab.url && tab.url.includes('youtube.com/watch')) {
-		const queryParameters = tab.url.split('?')[1];
-		const urlParameters = new URLSearchParams(queryParameters);
+console.log('background.js cargado');
 
-		chrome.tabs.sendMessage(tabId, {
-			type: 'NEW',
-			videoId: urlParameters.get('v'),
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+	if (changeInfo.url && changeInfo.url.includes('youtube.com/watch')) {
+		const queryParameters = changeInfo.url.split('?')[1];
+		const urlParameters = new URLSearchParams(queryParameters);
+		const videoId = urlParameters.get('v');
+
+		console.log('sendMessage NEW', videoId);
+
+		// Primero verifica si content.js está listo
+		chrome.tabs.sendMessage(tabId, { type: 'PING' }, (response) => {
+			if (chrome.runtime.lastError || !response) {
+				console.warn('content.js aún no está listo, esperando...');
+				setTimeout(() => {
+					chrome.tabs.sendMessage(tabId, { type: 'NEW', videoId: videoId });
+				}, 500);
+			} else {
+				// content.js está listo, enviamos el mensaje
+				chrome.tabs.sendMessage(tabId, { type: 'NEW', videoId: videoId });
+			}
 		});
 	}
 });
